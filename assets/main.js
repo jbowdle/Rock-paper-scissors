7fb8e7f70039e3a705d81dd2475faf8890f2cc1c
jbowdle <johnwbowdle@gmail.com>
2023-02-18 23:16:05 -0600

    Reset button now functional

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,8 @@ const tieDisplay = document.querySelector("#tie-display");
 const result = document.querySelector("#status");
 const playerImg = document.querySelector("#player-img");
 const compImg = document.querySelector("#comp-img");
+const statusDiv = document.querySelector("#status-div");
+const buttonReset = document.querySelector("#button-reset");
 
 function randomNum(max) {
     return Math.floor(Math.random() * max);
@@ -30,6 +32,7 @@ buttonRock.addEventListener(
     function () {
         choice = "rock";
         playerImg.setAttribute("src", "./assets/rock.png");
+        playerDisplay.textContent = choice;
     }
 )
 
@@ -38,6 +41,7 @@ buttonPaper.addEventListener(
     function () {
         choice = "paper";
         playerImg.setAttribute("src", "./assets/paper.png");
+        playerDisplay.textContent = choice;
     }
 )
 
@@ -46,27 +50,43 @@ buttonScissors.addEventListener(
     function () {
         choice = "scissors";
         playerImg.setAttribute("src", "./assets/scissors.png");
+        playerDisplay.textContent = choice;
     }
 )
 
 const gameLoop = function() {
     let compChoice = options[randomNum(3)];
 
-    compDisplay.textContent = `Chose: ${compChoice}`;
-    playerDisplay.textContent = `Chose: ${choice}`;
+    switch (compChoice) {
+        case "rock":
+            compImg.setAttribute("src", "./assets/rock.png");
+            break;
+        case "paper":
+            compImg.setAttribute("src", "./assets/paper.png");
+            break;
+        case "scissors":
+             compImg.setAttribute("src", "./assets/scissors.png");
+            break;
+    }
+
+    compDisplay.textContent = compChoice;
+    playerDisplay.textContent = choice;
     
     if (choice === compChoice) {
         stats.ties++;
+        statusDiv.setAttribute("class", "status-tie");
         tieDisplay.textContent = `Ties: ${stats.ties}`;
         result.textContent = "Tie";
     } else if ((choice === "rock" && compChoice === "scissors") ||
         (choice === "paper" && compChoice === "rock") ||
         (choice === "scissors" && compChoice === "paper")) {
         stats.wins++;
+        statusDiv.setAttribute("class", "status-win");
         winDisplay.textContent = `Wins: ${stats.wins}`;
         result.textContent = "Win";
     } else {
         stats.losses++;  
+        statusDiv.setAttribute("class", "status-loss");
         lossDisplay.textContent = `Losses: ${stats.losses}`; 
         result.textContent = "Loss"; 
     }
@@ -79,5 +99,27 @@ const gameLoop = function() {
 
 buttonShoot.addEventListener(
     'click',
-    (e) => (gameLoop())
+    (e) => gameLoop()
 ) 
+
+buttonReset.addEventListener(
+    'click',
+    function() {
+        playerImg.setAttribute("src", "./assets/blank.png");
+        compImg.setAttribute("src", "./assets/blank.png");
+
+        playerDisplay.textContent = "";
+        compDisplay.textContent = "";
+
+        result.textContent = "";
+
+        statusDiv.setAttribute("class", "status-reset");
+
+        stats.wins = 0;
+        stats.losses = 0;
+        stats.ties = 0;
+        winDisplay.textContent = `Wins: ${stats.wins}`;
+        lossDisplay.textContent = `Losses: ${stats.losses}`;
+        tieDisplay.textContent = `Ties: ${stats.ties}`;
+    }
+)
